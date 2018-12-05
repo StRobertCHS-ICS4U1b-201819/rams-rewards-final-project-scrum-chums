1f51728ef36842dd3113b103ceea4104a3c0553f
zhang11111111 <box.zhang01@gmail.com>
2018-12-05 18:45:38 -0500

    working build for the radmin django web app

--- Radmin/robsite/activities/models.py
+++ Radmin/robsite/activities/models.py
@@ -3,11 +3,7 @@ from django.db import models
 class Activity(models.Model):
     activity_name = models.CharField(max_length = 200)
     activity_score = models.IntegerField()
-    activity_date = models.DateField()
-    # '%Y-%m-%d'      '2006-10-25'
-    # '%m/%d/%Y'      '10/25/2006'
-    # '%m/%d/%y'      '10/25/06'
     
     def __str__(self):
-        return self.activity_name
+        return " ".join((self.activity_name, str(self.activity_score)))
    
--- Radmin/robsite/activities/views.py
+++ Radmin/robsite/activities/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic.list import ListView
+from .models import Activity 
+
+
+#class ActivityListView(ListView):
+#     model = Activity
 
 
 def index(request):
-    return HttpResponse("App for Activities")
+    lst = Activity.objects.all()
+    return HttpResponse([activity.__str__() for activity in lst])

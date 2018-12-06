79202a6244b7dcf788715290546f2e9c5eda3455
zhang11111111 <box.zhang01@gmail.com>
2018-12-06 13:10:43 -0500

    html formatting

--- Radmin/robsite/activities/models.py
+++ Radmin/robsite/activities/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import date
 
 class Activity(models.Model):
     activity_name = models.CharField(max_length = 200)
@@ -21,7 +22,10 @@ class OneTimeActivity(Activity):
     activity_date = models.DateField()
 	
     def get_date(self):
-        return self.date
+        return self.activity_date
+		
+    def is_past(self):
+        return date.today() > self.activity_date
 	
     def __str__(self):
         return " ".join((self.activity_name, str(self.activity_score), str(self.activity_date)))
--- Radmin/robsite/activities/views.py
+++ Radmin/robsite/activities/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from django.views.generic.list import ListView
 from django.template import Context, loader
 from .models import Activity, OneTimeActivity
-
+from datetime import date
 
 #class ActivityListView(ListView):
 #     model = Activity
@@ -11,9 +11,11 @@ from .models import Activity, OneTimeActivity
 
 def index(request):
 #    lst = [activity.__str__() for activity in Activity.objects.all()]
-    lst = Activity.objects.all()
+    all_list = Activity.objects.all()
     onetime = OneTimeActivity.objects.all()
+    upcoming = OneTimeActivity.objects.filter(activity_date__gte=date.today()).order_by("activity_date")
+    past = OneTimeActivity.objects.filter(activity_date__lt=date.today()).order_by("-activity_date")
 #    return HttpResponse([activity.__str__() for activity in lst])
-    template = loader.get_template("index.html")	
-    context = Context({"activity_list": lst})
-    return HttpResponse(template.render({'activity_list': lst, 'onetime_list': onetime}))
+    template = loader.get_template("index.html")
+    context = Context({"activity_list": all_list})
+    return HttpResponse(template.render({'activity_list': all_list, 'onetime_list': onetime, 'upcoming_list': upcoming, 'past_list': past}))

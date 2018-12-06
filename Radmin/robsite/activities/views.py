33873de1bd9754cf090bb424d62ecd7e21c5e376
zhang11111111 <box.zhang01@gmail.com>
2018-12-05 20:11:08 -0500

    display actually has html now

--- Radmin/robsite/activities/views.py
+++ Radmin/robsite/activities/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic.list import ListView
+from django.template import Context, loader
 from .models import Activity 
 
 
@@ -9,5 +10,9 @@ from .models import Activity
 
 
 def index(request):
+#    lst = [activity.__str__() for activity in Activity.objects.all()]
     lst = Activity.objects.all()
-    return HttpResponse([activity.__str__() for activity in lst])
+#    return HttpResponse([activity.__str__() for activity in lst])
+    template = loader.get_template("index.html")	
+    context = Context({"activity_list": lst})
+    return HttpResponse(template.render({'activity_list': lst}))

5622b4c7f33ea4cec81ef8fca7b67eb408a21b76
zhang11111111 <box.zhang01@gmail.com>
2018-12-07 11:07:17 -0500

    new classes for student

--- Radmin/robsite/activities/admin.py
+++ Radmin/robsite/activities/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from .models import Activity, OneTimeActivity
+from .models import Activity, OneTimeActivity, RepeatedActivity, Student
 
 admin.site.register(Activity)
+admin.site.register(RepeatedActivity)
 admin.site.register(OneTimeActivity)
+admin.site.register(Student)
--- Radmin/robsite/activities/models.py
+++ Radmin/robsite/activities/models.py
@@ -10,22 +10,45 @@ class Activity(models.Model):
 		
     def get_score(self):
         return self.activity_score
+
+    def rename(self, name):
+        self.activity_name = name
+    
+    def set_score(self, score):
+        self.activity_score = score
 		
-    def str_score(self):
-        return str(self.activity_score)
-	
     def __str__(self):
         return " ".join((self.activity_name, str(self.activity_score)))
 	
+
+class RepeatedActivity(Activity):
+    pass
+
 	
 class OneTimeActivity(Activity):
     activity_date = models.DateField()
 	
     def get_date(self):
         return self.activity_date
+
+    def set_date(self, date):
+        self.activity_date = date
 		
     def is_past(self):
         return date.today() > self.activity_date
 	
     def __str__(self):
         return " ".join((self.activity_name, str(self.activity_score), str(self.activity_date)))
+
+		
+class Student(models.Model):
+    GRADE_CHOICES = (("9", "9"), ("10", "10"), ("11", "11"), ("12", "12"))
+    student_name = models.CharField(max_length = 200)
+    student_grade = models.CharField(max_length=2, choices=GRADE_CHOICES, default="9")
+    student_score = 0
+
+    def add_score(self, score):
+        self.student_score += score
+		
+    def __str__(self):
+        return " ".join((self.student_name, self.student_grade, str(self.student_score)))

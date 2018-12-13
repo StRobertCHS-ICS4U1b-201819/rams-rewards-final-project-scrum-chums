79969da6682d9eaa90e55105e9ee08ae66ded116
zhang11111111 <box.zhang01@gmail.com>
2018-12-13 11:38:51 -0500

    updated activities in the database- excellent descriptions

--- Radmin/mysite/activities/urls.py
+++ Radmin/mysite/activities/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from django.views.generic import ListView, DetailView
 from activities.models import Activitie
 
-urlpatterns = [ path('', ListView.as_view(queryset=Activitie.objects.all().order_by("-date")[:25],
+urlpatterns = [ path('', ListView.as_view(queryset=Activitie.objects.all().order_by("date")[:25],
                                                      template_name="activities/activities.html")),
                 path('<int:pk>/', DetailView.as_view(model = Activitie,
                                                         template_name = "activities/post.html")),] 

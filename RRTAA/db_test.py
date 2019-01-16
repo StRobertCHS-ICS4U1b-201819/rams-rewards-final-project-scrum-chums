77aafce55c52433f745a813052057aaac4fe7b8f
zhang11111111 <box.zhang01@gmail.com>
2019-01-16 10:43:50 -0500

    more get functions

--- RRTAA/db_test.py
+++ RRTAA/db_test.py
@@ -1,10 +1,11 @@
 import sqlite3
 con = sqlite3.connect("db.sqlite3")
+# on return = [0] -> id, [1] -> name, [2] -> grade, [3] -> score, [4] -> id, [5] -> password
 
 
 def update_score(connection, param):
     try:
-        sql = "UPDATE students_student SET student_score = ? WHERE student_name = ?"  # sets the student score
+        sql = "UPDATE students_student SET student_score = ? WHERE student_id = ?"  # sets the student score
         # question marks are filled in order with the values in param
         # param must have the proper values or it will fail
         # add addition fields to SET using comma as separator "SET score = ?, name = ?"
@@ -32,7 +33,14 @@ def get_by_id(connection, student_id):
     return result
 
 
-def verifiy(connection, userid, password):
+def get_by_name(connection, student_id):
+    sql = "SELECT * FROM students_student WHERE student_name = ?"
+    cur = connection.cursor()
+    cur.execute(sql, (student_id,))
+    result = cur.fetchall()
+    return result
+
+def verify(connection, userid, password):
     sql = "SELECT * FROM students_student WHERE student_id = ? and student_pass = ?"
     cur = connection.cursor()
     cur.execute(sql, (userid, password))
@@ -42,12 +50,15 @@ def verifiy(connection, userid, password):
     return True
 
 
-# print(return_all(con))
-# update_score(con, (1, "Donnor Cong"))
-# print(return_all(con))
-id = "userid"
-ps = "password"
-get_by_id(con, "ballsDPcoder")
-if verifiy(con, id, ps):
-    student = get_by_id(con, id)[0]
-    print(" ".join(("Hello!", student[1])))
+if __name__ == "__main__":
+    # print(return_all(con))
+    # update_score(con, (1, "Donnor Cong"))
+    # print(return_all(con))
+    id = "userid"
+    ps = "password"
+    carson = get_by_id(con, "ballsDPcoder")[0]
+    print(carson[1])
+    if verify(con, id, ps):
+        student = get_by_id(con, id)[0]
+        print(" ".join(("Hello!", student[1])))
+        # Login Success

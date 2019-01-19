256fe5af53c91077ecf16a14dfc2af2b298e6edc
robuntu <robuntu@stro.ycdsb.ca>
2019-01-19 12:16:48 -0500

    finish add activity and history methods

--- Radmin/mysite/activities/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from activities.models import Activitie
-
-admin.site.register(Activitie)
--- Radmin/mysite/students/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path, include
-from django.views.generic import ListView, DetailView
-from students.models import Student
-
-urlpatterns = [ path('', ListView.as_view(queryset=Student.objects.all().order_by("-student_score")[:25],
-                                                     template_name="students/students.html"))] 

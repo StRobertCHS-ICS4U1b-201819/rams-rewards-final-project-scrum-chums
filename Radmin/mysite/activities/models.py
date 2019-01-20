d60af42c40b91356bf3f41768a8dcb713e88ea7d
Erin Chin <erin.chin19@ycdsbk12.ca>
2019-01-20 19:41:13 +0000

    updating erin-dev

--- Radmin/mysite/activities/models.py
+++ Radmin/mysite/activities/models.py
@@ -4,7 +4,7 @@ class Activitie(models.Model):
     title = models.CharField(max_length = 140)
     body = models.TextField()
     date = models.DateTimeField()
-    # points = models.DecimalField(max_digits = 10000, decimal_places = 0)
+    points = models.IntegerField()
     
     def __str__(self):
         return self.title
